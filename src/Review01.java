9f8ab98c344f87b85a6bc529bc9404445be97a2d
kentaakiyoshi <kakerulab@gmail.com>
2023-12-01 20:46:57 +0900

    javaプログラミング入門学習終了

--- src/Review01.java
+++ src/Review01.java
@@ -1,17 +1,16 @@
-
-
 public class Review01 {
 
-    public static void main(String[] args) {
-        //価格、税込価格、消費税の金額を変数として定義
-        int productPrice=1500; //商品価格（例：1000円）
-        int taxIncludedPrice=1650; //税込価格（例：1100円）
-        int taxAmount=150; //消費税の金額（例：100円）
-
-        // 結果の出力
-        System.out.println(productPrice + "円の商品の税込価格は：" +taxIncludedPrice + "円（消費税は"+taxAmount+"円です）。");
-
-
+    // 消費税を計算するメソッド
+    public static int tax(int price) {
+        return (int) (price * 0.10); // 10%の消費税を計算
     }
 
+    public static void main(String[] args) {
+        int productPrice = 1500; // 商品価格
+        int taxAmount = tax(productPrice); // 消費税の計算
+        int taxIncludedPrice = productPrice + taxAmount; // 税込価格の計算
+
+        // 結果の出力
+        System.out.println(productPrice + "円の商品の税込価格は：" + taxIncludedPrice + "円（消費税は" + taxAmount + "円です）。");
+    }
 }
